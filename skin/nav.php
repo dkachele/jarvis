a06905a4dc79eea6b0864c7dbdb4ef8498c09bd6
Dale Kachele <dkachele@gmail.com>
2021-04-01 00:29:47 -0400

    Added tools vs admin

--- skin/nav.php
+++ skin/nav.php
@@ -1,5 +1,13 @@
 <nav id="sidebarMenu" class="col-md-4 col-lg-3 d-md-block bg-light sidebar collapse">
 	<div class="position-sticky pt-3">
+		<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">Tools</h6>
+		<ul class="nav flex-column">
+			<li class="nav-item">
+				<a class="nav-link <?php if ($mode === 'propsals') echo "active"; ?>" href="/?mode=proposals">Proposals</a>
+			</li>
+		</ul>
+
+		<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">Admin</h6>
 		<ul class="nav flex-column">
 			<?php foreach ($nav_elements as $nav_element) { ?>
 				<li class="nav-item">
